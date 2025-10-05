a6009e7c4a8cefa3d721a0e9a59978425eeb110c
KellyShaun <kellywambui05@gmail.com>
2025-10-05 17:36:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,20 @@
-const CACHE_NAME = 'tracker-cache-v2';
-const urlsToCache = ['./', './index.html'];
+const CACHE_NAME = 'bunny-tracker-cache-v1';
+const urlsToCache = ['./', './Tracker.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
-// Install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); // 🔥 Activate new worker immediately
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
-// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
     )
   );
-  self.clients.claim(); // 🔥 Refresh clients to use new cache
+  self.clients.claim();
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
